053c6f61be45bc6349a7af0f3f28f255a1e25094
Menna-Emad <https://github.com/Menna-Emad>
2024-02-16 02:41:40 +0200

    second commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
+
+
